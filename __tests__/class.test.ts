1b83e45be7c791b560482c9924c2fa504f3fa356
gtobinaga-sparkleai <gtobinaga@sparkleai.co.jp>
2024-06-24 22:22:26 +0900

    passed all tests

--- __tests__/class.test.ts
+++ __tests__/class.test.ts
@@ -2,9 +2,14 @@ import { Car, CarShop } from '../src/class';
 
 describe('CarShop', () => {
   let carShop: CarShop;
-
+  const car1 = new Car("Lancer EVO VIII", "Mitsubishi", "4WD", "foiaejoiawf90");
+  const car2 = new Car("MR2", "Toyota", "MR", "fjf038fj08");
+  const car3 = new Car("86", "Toyota", "FR", "o234ufh948f");
   beforeEach(() => {
     carShop = new CarShop();
+    carShop.addCar(car1);
+    carShop.addCar(car2);
+    carShop.addCar(car3);
   });
 
   it('should add a car', () => {
@@ -14,13 +19,23 @@ describe('CarShop', () => {
   });
 
   it('should list all cars', () => {
-    const car1 = new Car("Lancer EVO VIII", "Mitsubishi", "4WD", "foiaejoiawf90");
-    const car2 = new Car("MR2", "Toyota", "MR", "fjf038fj08");
-    carShop.addCar(car1);
-    carShop.addCar(car2);
     const allCars = carShop.listAllCars();
-    expect(allCars).toHaveLength(2);
+    expect(allCars).toHaveLength(3);
     expect(allCars).toContain(car1);
     expect(allCars).toContain(car2);
+    expect(allCars).toContain(car3)
   });
+
+  it('should return searched Car object', () => {
+    const lancer = carShop.findCarByName("Lancer EVO VIII");
+    expect(lancer).toEqual(car1);
+  });
+
+  it('should return this brand cars objects', () => {
+    const toyotaCars = carShop.findCarByBrand("Toyota");
+    expect(toyotaCars).toHaveLength(2);
+    expect(toyotaCars).toContain(car2);
+    expect(toyotaCars).toContain(car3);
+    expect(toyotaCars).not.toContain(car1);
+  })
 });
